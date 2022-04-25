2234c9c232c7c7e36abe3016b21c87bab423c8fa
AlexGonzalezI <agonzalezi2006@gmail.com>
2022-04-25 23:45:32 +0200

    Boton Welcome Acabado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <div className="welcome">
+        WELCOME  
+      </div>
+      <div className='cajaMedioContainer'>
+        <div className='cajaMedio'> 
+          <div  className="greetingText">
+            Hola Tinsky  
+            </div>  
+            <div  className="greetingText">
+            GL  
+          </div>  
+        </div>
+      </div>
     </div>
   );
 }
